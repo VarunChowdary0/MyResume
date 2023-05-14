e3958d36bfdd410e063a2f8b60d39ddcf8ceb165
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2023-05-15 00:16:38 +0530

    Adding to GitHub

---
+++ index.js
@@ -0,0 +1,31 @@
+const express=require('express')
+const app=express();
+
+app.use(express.static('public'))
+app.set('view engine','ejs')
+app.set('views','./views')
+
+app.get('/home',(req,res)=>{
+    res.render('home');
+})
+app.get('/about',(req,res)=>{
+    res.render('about');
+})
+ app.get('/contact',(req,res)=>{
+    res.render('contact');
+})
+ app.get('/education',(req,res)=>{
+     res.render('education');
+})
+ app.get('/experience',(req,res)=>{
+     res.render('experience');
+})
+ app.get('/skills',(req,res)=>{
+    res.render('skills');
+ })
+ app.get('/myProjects',(req,res)=>{
+    res.render('myProjects')
+})
+app.listen(1000,()=>{
+    console.log("Server running on port 1000............")
+})
---
+++ public/js/resume.js
@@ -0,0 +1,42 @@
+const dropDown=()=>{
+    let dropBox=document.querySelector(".magic").innerHTML=`
+    <div class="menu_Button" onclick="UNdrop()">
+        <img src="icons/menu.png" alt="">
+        <div class="SideDrop">
+        <div class="menus">
+        <a href="/home">Home</a>
+        </div>
+        <div class="menus">
+            <a href="/about">About</a>
+        </div>
+        <div class="menus">
+            <a href="/skills">Skills</a>
+        </div>
+        <div class="menus">
+            <a href="/experience">Experience</a>
+        </div>
+        <div class="menus">
+            <a href="/education">Education</a>
+        </div>
+        <div class="menus">
+            <a href="myProjects">My Projects</a>
+        </div>
+        <div class="menus">
+            <a href="/contact">Contact</a>
+        </div>
+            </div>
+        </div>
+  `;
+  let left=document.querySelector(".left");
+  left.style.filter='blur(4px)';
+  let right=document.querySelector(".right");
+  right.style.filter='blur(4px)';
+}
+
+const UNdrop=()=>{
+    let undrop=document.querySelector(".magic").innerHTML=`<div class="menu_Button" onclick="dropDown()"><img src="icons/menu.png" alt=""></div>`;
+    let left=document.querySelector(".left");
+    left.style.filter='blur(0px)';
+    let right=document.querySelector(".right");
+    right.style.filter='blur(0px)';
+}
